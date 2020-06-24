e88812fd28c37101be832ef64a13ba7755719c9f
Elco Garcia <garciaelco18@gmail.com>
2020-06-23 21:50:26 -0500

    Created struct for stories  and method for changing stories

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
 
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        //figure out function to change choices
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct Story{
+    var title: String
+    let choice1: String
+    let choice2: String
+}
+
+let stories = [Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right."),Story(title: "You see a tiger", choice1: "SHout for help", choice2: "Play dead."),Story(title: "You find a treasure chest", choice1: "Open it.", choice2: "Check for traps")]
+
+let example = Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right.")
